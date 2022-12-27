fe456e52d24414b7837521d1068cf6b003da66bf
Angelos <v.pazaitis@web.de>
2022-12-28 00:15:31 +0100

    anpassung

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -28,14 +28,14 @@
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         Dein Input
       </label>
-      <input class="h-40 w-60 appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+      <input class="h-40 w-60 appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="80qm, 4 Zimmer, Wohnung, mit Stellplatz" value="80qm, 4 Zimmer, Wohnung, mit Stellplatz">
       <p class="text-red-500 text-xs italic">Fülle dies aus!</p>
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
         Unser Expose
       </label>
-      <input class="h-40 w-60 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+      <input class="h-40 w-60 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Klicke auf den Button!">
     </div>
   </div>
  
