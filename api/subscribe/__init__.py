f66ea46b181ba976b4e863075bd1b4cac3e5f1c0
Angelos <v.pazaitis@web.de>
2022-12-28 00:46:13 +0100

    test

--- api/subscribe/__init__.py
+++ api/subscribe/__init__.py
@@ -4,6 +4,13 @@ import json
 import azure.functions as func
 
 
+import os
+import openai
+
+
+
+
+
 def main(req: func.HttpRequest, outputTable: func.Out[str]) -> func.HttpResponse:
     logging.info('createPage HTTP trigger function processed a request.')
 
@@ -17,10 +24,20 @@ def main(req: func.HttpRequest, outputTable: func.Out[str]) -> func.HttpResponse
             "RowKey": rowKey
         }
 
+        # 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
+
         outputTable.set(json.dumps(data))
         
+        openai.api_key = "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"
+        openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Schreibe ein expose for eine 40qm wohnung in Köln mit Garage",
+        max_tokens=7,
+        temperature=0
+        )
+
         return func.HttpResponse(
-            f"Subscribed with {email}!",
+            f"Subscribed with {openai.toString()}!",
             status_code=201
         )
     else:
