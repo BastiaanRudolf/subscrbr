8620cc649c638323160859b9a08cd01a7a4342af
Angelos <v.pazaitis@web.de>
2022-12-28 00:15:16 +0100

    zwei spalten

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -21,6 +21,27 @@
         Probiere es jetzt. 👇
       </p>
 
+
+      <form class="w-full max-w-lg">
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        Dein Input
+      </label>
+      <input class="h-40 w-60 appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+      <p class="text-red-500 text-xs italic">Fülle dies aus!</p>
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+        Unser Expose
+      </label>
+      <input class="h-40 w-60 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+    </div>
+  </div>
+ 
+  
+</form>
+
       <!-- Form with button -->
       <div>
         <div class="flex w-full justify-center items-end">
